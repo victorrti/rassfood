81974ff7048e53bee311bbfeef2bc412e9493991
victorrti <victorrodriguesti@gmail.com>
2024-10-11 08:07:00 -0300

    criando classe de ordemservice

---
+++ src/main/java/br/com/rasfood/service/teste/OrdemService.java
@@ -0,0 +1,15 @@
+package br.com.rasfood.service.teste;
+
+import br.com.rasfood.utils.JPAutils;
+import br.com.rasfood.utils.Mock;
+
+import javax.persistence.EntityManager;
+
+public class OrdemService {
+    public static void main(String[] args) {
+        EntityManager entityManager = JPAutils.getEntityManagerrestaurante();
+        entityManager.getTransaction().begin();
+        Mock.cadastrarCategoria(entityManager);
+        Mock.cadastrarProdutoCardapio(entityManager);
+    }
+}
