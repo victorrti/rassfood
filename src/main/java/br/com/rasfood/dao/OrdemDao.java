608da4240e95af8042bfb13793207baa439a3976
victorrti <victorrodriguesti@gmail.com>
2024-10-11 08:04:23 -0300

    criando dao de ordem com os metodos de crud

---
+++ src/main/java/br/com/rasfood/dao/OrdemDao.java
@@ -0,0 +1,37 @@
+package br.com.rasfood.dao;
+
+import br.com.rasfood.entity.Categoria;
+import br.com.rasfood.entity.Ordem;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class OrdemDao {
+    private EntityManager entityManager;
+
+    public OrdemDao(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+    public void cadastrar(Categoria categoria){
+        this.entityManager.persist(categoria);
+        System.out.println(categoria.getId());
+    }
+
+    public Ordem consultar(Integer id){
+        return this.entityManager.find(Ordem.class,id);
+    }
+
+    public void atualizar(Ordem ordem){
+        this.entityManager.merge(ordem);
+    }
+
+    public void excluir(Ordem ordem){
+        this.entityManager.remove(ordem);
+    }
+
+    public List<Ordem> findAll(){
+
+        String sql = "SELECT o FROM Ordem o";
+        return this.entityManager.createQuery(sql,Ordem.class).getResultList();
+    }
+}
